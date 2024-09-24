d93b79f8cd6a7fe90047bb56b58469087654a382
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-24 16:28:50 -0300

    Testes do componente like-widget

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { LikeWidgetComponente } from "./like-widget.component";
-import { UniqueIdService } from "../../services/unique-id/unique-id.service";
+import { LikeWidgetModule } from "./like-widget.module";
 
 describe(LikeWidgetComponente.name, () => {
-    let fixture: ComponentFixture<LikeWidgetComponente> = null
+    let fixture: ComponentFixture<LikeWidgetComponente> = null;
+    let component: LikeWidgetComponente = null;
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LikeWidgetComponente],
-            imports: [FontAwesomeModule],
-            providers: [UniqueIdService]
+            imports: [LikeWidgetModule]
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponente);
-    })
+        component = fixture.componentInstance;
+    });
 
     it('Should be created component', () => {
-        const instance = fixture.componentInstance;
-        expect(instance).toBeTruthy();
+        expect(component).toBeTruthy();
+    });
+
+    it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
+        fixture.detectChanges();
+        expect(component.id).toBeTruthy();
+    });
+
+    it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
+        component.id = 'someId';
+        fixture.detectChanges();
+        expect(component.id).toBe('someId');
     })
+
+    it(`#${LikeWidgetComponente.prototype.like.name} should trigger (@Output liked) when called`, () => {
+        spyOn(component.liked, `emit`)
+        fixture.detectChanges();
+        component.like();
+        expect(component.liked.emit).toHaveBeenCalled();
+    });
 })
